3493ca8325f9f94b8b32911d0fe797b5224971f5
User <User@DESKTOP-6G1M4V4>
2018-05-21 15:20:14 -0300

    asdf

--- TCC/src/Telas/TelaPrincipal.java
+++ TCC/src/Telas/TelaPrincipal.java
@@ -6,6 +6,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JSeparator;
 import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.Panel;
 
 public class TelaPrincipal {
 
@@ -55,8 +61,68 @@ public class TelaPrincipal {
 		frmPrincipal.getContentPane().add(separator);
 		
 		JButton btnCadastrarLoja = new JButton("Cadastrar Loja");
+		btnCadastrarLoja.setBackground(Color.LIGHT_GRAY);
 		btnCadastrarLoja.setFocusable(false);
-		btnCadastrarLoja.setBounds(192, 117, 141, 23);
+		btnCadastrarLoja.setBounds(338, 83, 138, 23);
 		frmPrincipal.getContentPane().add(btnCadastrarLoja);
+		
+		JButton btnCadastrarUsuario = new JButton("Cadastrar Usu\u00E1rio");
+		btnCadastrarUsuario.setBackground(Color.LIGHT_GRAY);
+		btnCadastrarUsuario.setFocusable(false);
+		btnCadastrarUsuario.setBounds(338, 117, 138, 23);
+		frmPrincipal.getContentPane().add(btnCadastrarUsuario);
+		
+		JButton btnCliente = new JButton("Cliente");
+		btnCliente.setBackground(SystemColor.menu);
+		btnCliente.setFocusable(false);
+		btnCliente.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnCliente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCliente.setBounds(10, 164, 89, 23);
+		frmPrincipal.getContentPane().add(btnCliente);
+		
+		JButton btnCaixa = new JButton("Caixa");
+		btnCaixa.setBackground(SystemColor.menu);
+		btnCaixa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCaixa.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnCaixa.setFocusable(false);
+		btnCaixa.setBounds(10, 198, 89, 23);
+		frmPrincipal.getContentPane().add(btnCaixa);
+		
+		JButton btnEstoque = new JButton("Estoque");
+		btnEstoque.setBackground(SystemColor.menu);
+		btnEstoque.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnEstoque.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnEstoque.setFocusable(false);
+		btnEstoque.setBounds(10, 232, 89, 23);
+		frmPrincipal.getContentPane().add(btnEstoque);
+		
+		JButton btnSair = new JButton("Sair");
+		btnSair.setBackground(SystemColor.menu);
+		btnSair.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnSair.setFocusable(false);
+		btnSair.setBounds(10, 266, 89, 23);
+		frmPrincipal.getContentPane().add(btnSair);
+		
+		Panel pnCliente = new Panel();
+		pnCliente.setBounds(152, 159, 324, 148);
+		frmPrincipal.getContentPane().add(pnCliente);
+		pnCliente.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Clientes");
+		btnNewButton.setBounds(10, 11, 59, 48);
+		pnCliente.add(btnNewButton);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 	}
 }
