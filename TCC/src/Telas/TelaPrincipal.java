065033f0b527cc62785d363e35de6d90f02163ac
User <User@DESKTOP-6G1M4V4>
2018-05-26 21:31:01 -0300

    aszx

--- TCC/src/Telas/TelaPrincipal.java
+++ TCC/src/Telas/TelaPrincipal.java
@@ -65,19 +65,6 @@ public class TelaPrincipal {
 		frmPrincipal.setLocationRelativeTo(null);
 		frmPrincipal.getContentPane().setLayout(null);
 		
-		JButton btnSair = new JButton("Sair");
-		btnSair.setForeground(Color.WHITE);
-		btnSair.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frmPrincipal.dispose();
-			}
-		});
-		btnSair.setBackground(Color.decode("#0049aa"));
-		btnSair.setFont(new Font("Impact", Font.PLAIN, 13));
-		btnSair.setFocusable(false);
-		btnSair.setBounds(10, 275, 89, 23);
-		frmPrincipal.getContentPane().add(btnSair);
-		
 		JLabel lblLogo = new JLabel("");
 		lblLogo.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLogo.setIcon(new ImageIcon(TelaPrincipal.class.getResource("/SIG 129.png")));
@@ -214,6 +201,19 @@ public class TelaPrincipal {
 		btnEstoque.setFocusable(false);
 		btnEstoque.setBounds(10, 241, 89, 23);
 		frmPrincipal.getContentPane().add(btnEstoque);
+		
+		JButton btnSair = new JButton("Sair");
+		btnSair.setForeground(Color.WHITE);
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frmPrincipal.dispose();
+			}
+		});
+		btnSair.setBackground(Color.decode("#0049aa"));
+		btnSair.setFont(new Font("Impact", Font.PLAIN, 13));
+		btnSair.setFocusable(false);
+		btnSair.setBounds(10, 275, 89, 23);
+		frmPrincipal.getContentPane().add(btnSair);
 		
 		pnCliente = new JPanel();
 		pnCliente.setOpaque(false);
