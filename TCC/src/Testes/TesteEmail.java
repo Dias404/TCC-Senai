15f3f59ab99da7cfcbdea114b982e49bd09e0a63
User <User@DESKTOP-6G1M4V4>
2018-05-28 12:21:04 -0300

    asdf

--- TCC/src/Testes/TesteEmail.java
+++ TCC/src/Testes/TesteEmail.java
@@ -1,10 +1,33 @@
 package Testes;
 
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
 public class TesteEmail {
 
-	public 
+	public String enviarEmail() {
+		try {
+			SimpleEmail enviarEmail = new SimpleEmail();
+			enviarEmail.setHostName("smtp.gmail.com");
+			enviarEmail.setSmtpPort(465);
+			enviarEmail.setAuthentication("projetojavasenai@gmail.com", "senai123");
+			enviarEmail.setSSLOnConnect(true);
+			enviarEmail.setFrom("projetojavasenai@gmail.com");
+			enviarEmail.setSubject("Teste");
+			enviarEmail.setMsg("Teste: Mandar Email - TCC: Projeto SIG");
+			enviarEmail.addTo("ruldi.borges@gmail.com");
+			enviarEmail.send();
+			return "E-mail enviado";
+		} catch (EmailException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "Fail";
+		}
+		
+	}
 	
 	public static void main(String[] args) {
+		System.out.println(new TesteEmail().enviarEmail());
 	}
 
 }
