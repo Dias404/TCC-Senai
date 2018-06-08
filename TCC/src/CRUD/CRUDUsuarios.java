1b58352ac6a3465d529b78cfa8fb1d807b5e2c3a
SENAI <SENAI@SENAI-05>
2018-06-08 13:50:27 -0300

    asdf

---
+++ TCC/src/CRUD/CRUDUsuarios.java
@@ -0,0 +1,50 @@
+package CRUD;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Banco.Conexao;
+import DAO.Clientes;
+import DAO.Usuarios;
+
+public class CRUDUsuarios {
+
+	Conexao con = new Conexao();
+	public ResultSet dadosSelect = null;
+	
+	public boolean insertUsuario(Usuarios usuario) {
+		try {
+			String sql = "INSERT INTO usuarios (nome, senha, email, fone, cel) VALUES (?, ?, ?, ?, ?)";
+			PreparedStatement stmt = con.getConexao().prepareStatement(sql);
+			stmt.setString(1, usuario.getNome());
+			stmt.setString(2, usuario.getSenha());
+			stmt.setString(3, usuario.getEmail());
+			stmt.setString(4, usuario.getFone());
+			stmt.setString(5, usuario.getCel());
+			stmt.execute();
+			stmt.close();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public ResultSet selectUsuarios() {
+		String sql = "SELECT * FROM usuarios";
+		try {
+			PreparedStatement stmt = con.getConexao().prepareStatement(sql);
+			dadosSelect = stmt.executeQuery();
+			stmt.execute();
+			stmt.close();
+			return dadosSelect;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return dadosSelect = null;
+		}
+	}
+	
+}
---
+++ TCC/src/DAO/Usuarios.java
@@ -0,0 +1,49 @@
+package DAO;
+
+public class Usuarios {
+
+	public int idUsuarios;
+	public String nome;
+	public String senha;
+	public String email;
+	public String fone;
+	public String cel;
+	
+	public int getIdUsuarios() {
+		return idUsuarios;
+	}
+	public void setIdUsuarios(int idUsuarios) {
+		this.idUsuarios = idUsuarios;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFone() {
+		return fone;
+	}
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+	public String getCel() {
+		return cel;
+	}
+	public void setCel(String cel) {
+		this.cel = cel;
+	}
+	
+}
